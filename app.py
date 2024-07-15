2d8d5116702f823dd405f1d95c15d2eef62e83a7
Sruthy K S <141996444+SruthyK-S@users.noreply.github.com>
2024-07-15 14:42:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
 import streamlit as st
+import requests
 
-st.set_page_config(
-    page_title="Food Recommendation System",
-    page_icon="🍓",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-st.title("Food Recommendation")
-st.write("""Craving something delicious but stuck in decision fatigue? Enter the food recommendation system,
-        your culinary compass! Tell it your tastes, dietary needs, and the occasion, and watch as it conjures up personalized dish 
-        suggestions. Spicy Thai curry for a solo weeknight? Check. Elegant pasta for a romantic date? Double check. 
-        Never guess again—let AI guide you to your next taste bud adventure!""")
-images = ["https://images.pexels.com/photos/1435735/pexels-photo-1435735.jpeg", 
-          "https://images.pexels.com/photos/1854652/pexels-photo-1854652.jpeg?auto=compress&cs=tinysrgb&w=600", 
-          "https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&w=600"]
+st.title("Recipe Generator")
 
-with st.sidebar:
-    st.image(images)
-col = st.columns(3)
-for i in range(3):
-    with col[i]:
-        st.image(images[2-i])
-        if i == 0:
-            st.write("""No more "what's for dinner?" dread. Find hidden gems and forgotten favorites, 
-                     rediscover the joy of cooking, or explore new cuisines with confidence.""")
+def generate_recipe(ingredients):
+    """Generates a recipe based on given ingredients using a recipe API."""
+
+    api_key = "41ffcb2d914e47269044d6b7af34c4dc"
+    base_url = "https://api.spoonacular.com/recipes/findByIngredients"
+    params = {
+        "apiKey": api_key,
+        "ingredients": ",".join(ingredients),
+        "number": 1,  
+        "addRecipeInformation": True, 
+        "fillIngredients": True,  
+    }
+
+    response1 = requests.get(base_url, params=params)
+    data = response1.json()
+    
+    if len(data) > 0:
+        recipe = data[0]
+        
+        st.header(recipe["title"])
+        st.image(recipe["image"], width=400)
+
+        if recipe.get("missedIngredients"):
+                st.subheader("Missing Ingredients:")
+                s = ""
+                for i in recipe["missedIngredients"]:
+                    s = s + i['name'] + ", "
+                st.write(s)
+        st.subheader("Instructions:")
+        id = recipe["id"]
+        url = "https://api.spoonacular.com/recipes/{}/analyzedInstructions".format(id)
+        response2 = requests.get(url, {"apiKey" : api_key})
+        instructions = response2.json()
+        instruction = instructions[0]
+        for i in instruction['steps']:
+            st.write(i['step'])
+
+    else:
+        st.write("No recipes found with those ingredients.")
+
+ingredients = st.text_input("Enter ingredients separated by commas:")
+if st.button('Show Recipe'):
+    if ingredients:
+        ingredients_list = ingredients.split(",")
+        generate_recipe(ingredients_list)
+    else:
+        st.write("You have not entered any ingredients!")
+    
